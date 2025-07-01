6bd7e99400cbd9f193b31bb003d48de734b5c33f
wcffff <wcf20020326@sjtu.edu.cn>
2025-07-01 16:59:12 +0800

    Prevent misoperation in the SAM locking mode

--- BrakeControlSystemGUI/modules/gui.py
+++ BrakeControlSystemGUI/modules/gui.py
@@ -356,7 +356,7 @@ class BrakeControlSystemGUI(QMainWindow, Ui_Form):
                             "TRACK": track_id,
                             "CMD": cmd
                         }
-                        if not self.local_status[track_id :]:
+                        if not self.local_status[track_id]:
                             self.tcp_clients[track_id].send_downlink_command.emit(command)
                         func_track_map[function].add(track_id)
 
